18fb8371ffb2b80efa8abeaf8f1594db5e7a43de
ajay@magnetontech <ajay@magnetontech.com>
2024-03-26 22:02:46 +0530

    upalod files

---
+++ assets/js/layout.js
@@ -0,0 +1,1 @@
+!function(){"use strict";"dark-mode-switch"==sessionStorage.getItem("is_visited")?document.documentElement.setAttribute("data-bs-theme","dark"):"light-mode-switch"!=sessionStorage.getItem("is_visited")&&"rtl-mode-switch"!=sessionStorage.getItem("is_visited")||document.documentElement.setAttribute("data-bs-theme","light")}();
